f68a879e190a98a6ea8f5f82795fd48e6dc2e39a
pawor <pawor.emi@gmail.com>
2021-10-04 00:07:07 +0700

    add layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React  from 'react';
 import Head from 'next/head';
-import { AppBar } from '@material-ui/core';
+import useStyles from '../utils/styles';
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 
-export default function Layout() {
+export default function Layout({children}) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazon</title>
       </Head>
-      <AppBar position="static"></AppBar>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Typography>amazon</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>
+        {children}
+      </Container>
+      <footer className={classes.footer}>
+        <Typography>
+          All right reserved. Next Amazon.
+        </Typography>
+      </footer>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    
+    <Layout></Layout>
   )
 }
